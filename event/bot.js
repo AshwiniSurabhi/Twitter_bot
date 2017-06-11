3a7c8aae6b68339d101ebcc8d420a9d7e4f43b9e
Aswini <ashwinisurabhi@gmail.com>
2017-06-11 14:07:39 -0700

    Create bot.js

---
+++ event/bot.js
@@ -0,0 +1,28 @@
+var Twit = require("twit");
+var config = require('./config');
+var T = new Twit(config);
+
+var stream = T.stream('user');
+stream.on('follow', followed);
+
+function followed(event_data){
+	console.log("Working on follow_bot");
+	var name = event_data.source.name;
+	var screenName = event_data.source.screen_name;
+	TweetIt("hello"+name+"welcome here"+screen_name);
+
+}
+
+function TweetIt(txt){
+	var data_to_put = { 'status': txt }
+
+	T.post('statuses/update', data_to_put, putData)
+
+	function putData(err, data, response){
+		if(err)
+			console.log(err)
+		else
+			console.log(data.text);
+	}
+
+}
